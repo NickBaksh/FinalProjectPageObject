9795f43979d1bdd51d227e5e0694b262a46a25d5
NickBaksh <kote981030@gmail.com>
2022-04-25 11:04:19 +0300

    delete some imports

--- test_product_page.py
+++ test_product_page.py
@@ -5,7 +5,7 @@ from faker import Faker
 import pytest
 
 
-'''
+@pytest.mark.need_review
 @pytest.mark.parametrize('promo', ["1", "2", "3", "4", "5", "6", pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, promo):
     link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{promo}"
@@ -16,6 +16,7 @@ def test_guest_can_add_product_to_basket(browser, promo):
     page.elements_should_be_presented()
     page.assert_added_book_have_the_same_name()
     page.assert_same_price_in_basket_and_on_the_page()
+
 
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -40,31 +41,31 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page.should_dissapear_of_success_message()
 
 
-
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
 
-
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.open_basket()
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.basket_is_empty()
     basket_page.basket_is_empty_message()
-'''
+
 
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(scope="function", autouse=True)
@@ -85,6 +86,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1"
         page = ProductPage(browser, link)
@@ -94,3 +96,4 @@ class TestUserAddToBasketFromProductPage():
         page.elements_should_be_presented()
         page.assert_added_book_have_the_same_name()
         page.assert_same_price_in_basket_and_on_the_page()
+
